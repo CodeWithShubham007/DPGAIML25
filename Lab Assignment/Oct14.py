3d49388521773412f29823d424a809a68d434e49
Ershubham2908 <shubhamprepbytes007@gmail.com>
2025-10-14 09:33:32 +0530

    lab question

--- Lab Assignment/Oct14.py
+++ Lab Assignment/Oct14.py
@@ -1,5 +1,5 @@
 # Lab Questions
 
-# Q1. Imagine you are arranging students in a line according to their height.You keep comparing two students standing next 
-# to each other — if the shorter one is behind, you swap their position. You keep doing this again and again until everyone 
-# is standing in order from shortest to tallest.
+# Q1. Imagine you are arranging students in a line according to their height.You keep comparing two students standing next  to each other — if the shorter one is behind, you swap their position. You keep doing this again and again until everyone is standing in order from shortest to tallest.
+# Q2. Imagine you are holding playing cards in your hand. You pick one card at a time from the deck and insert it into the correct position in your already sorted hand.
+# Q3. Think of choosing the cheapest item from a store’s price list each time. You first pick the item with the lowest price, remove it, then pick the next lowest, and keep going until all are sorted.
