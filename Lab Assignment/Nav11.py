4a67b051536f5a8634c048d935581ccf2525ba0a
Ershubham2908 <shubhamprepbytes007@gmail.com>
2025-11-11 09:19:00 +0530

    Lab 11 Nov

---
+++ Lab Assignment/Nav11.py
@@ -0,0 +1,5 @@
+# 1. Create a Python function to check whether a given number is prime or not.
+# 2. Write a Python program to sort a list of integers using the bubble sort algorithm.
+# 3. Design a Python program to calculate the area of different geometric shapes (circle, rectangle, triangle)
+# 4. Write a Python function to search for a specific element in a list using linear search.
+# 5. Develop a Python program to implement a simple calculator using functions for addition, subtraction, multiplication, and division.
