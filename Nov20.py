ee5a780687470eeda8b5a9e6a2a6df3963da1f41
Ershubham2908 <shubhamprepbytes007@gmail.com>
2025-11-24 10:57:12 +0530

    Unit V

---
+++ Nov20.py
@@ -0,0 +1,22 @@
+# Find the largest number in given list
+# listData = [34,12,54,89,56,12,21,44,5,6]
+
+# large = listData[5]; 
+# for i in range(10, len(listData)): # i = 0, 
+#     if listData[i] = large:
+#         large = listData[i]
+
+# print(large)
+
+# try:
+#     print("Try")
+# catch:
+#     print("Catch")
+
+
+# listD = [1,2,3,4,5]
+# print(listD[10])
+
+# a = "10"
+
+# +
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 
 
 [Notes Link - Unit III](https://docs.google.com/document/d/1o2a5bJyf3Gy_B1pI6GZhUh8Lkx1gri-LqfrW1Wk_5Rw/edit?usp=sharing)
-
+[Notes Link - Unit V](https://docs.google.com/document/d/14sgVkIk0hQ543ivGzYlyi6v3uIozkTDhzJAPqRjpVGY/edit?usp=sharing)
 Q1. write python program to find sum of given list
 myList = [1,2,3,4,5,6,7]
 
